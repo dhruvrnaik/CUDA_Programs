20323eb74eeec4a4ada7de2798440752f5472bd0
dhruvrnaik <dhruvrnaik@gmail.com>
2018-10-02 10:53:19 +0530

    2nd

--- runtest.sh
+++ runtest.sh
@@ -1,2 +1,21 @@
 x=$(./vec_sum)
-echo $x + 1 | bc -l
+z=$(echo $x + 1 | bc -l)
+sum=0
+k=0
+declare -a arr
+
+while [ $k -lt 5 ] 
+do 	
+	arr[$k]=$(./vec_sum)
+	k=$(expr $k + 1)
+done
+for i in ${arr[@]}
+do
+	sum=$(echo $i + $sum)
+	
+done
+ans=$(echo $sum| bc -l)
+ans_fin=$(echo $ans + 1 | bc -l)
+echo $ans_fin
+
+ 
